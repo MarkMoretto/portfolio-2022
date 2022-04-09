dfa4fe4712f27842fd48dd98374daf1018ef331f
mark moretto <otteromkram@gmail.com>
2022-04-09 18:58:49 -0500

    added home, about components; updated routing in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+
+// https://reactrouter.com/docs/en/v6/upgrading/v5#upgrade-all-switch-elements-to-routes
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
 import './App.css';
 
+import Home from "./components/Home"
+import About from "./components/About"
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/about">About</Link>
+						</li>					
+					</ul>
+				</nav>
+			</div>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />					
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/About/index.tsx
@@ -0,0 +1,15 @@
+
+import styles from "About.module.css"
+
+const About = () => {
+    return (
+        <div className={styles.divStyle}>
+            <h2>About Mark</h2>
+            <main>
+                <p>What about Mark?  Who's askin'?</p>
+            </main>
+        </div>
+    )
+}
+
+export default About
