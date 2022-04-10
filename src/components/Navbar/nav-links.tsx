de960dfafe3c03c83800c1510bd47715396d3af3
mark moretto <otteromkram@gmail.com>
2022-04-10 16:04:01 -0500

    iojfeojf

---
+++ src/components/Navbar/nav-links.tsx
@@ -0,0 +1,21 @@
+
+
+const navLinks = [
+    {
+        id: 1,
+        path: "/",
+        text: "Home",
+    },
+    {
+        id: 2,
+        path: "/pages",
+        text: "Pages",
+    },	
+    {
+        id: 3,
+        path: "/about",
+        text: "About",
+    }
+]
+
+export default navLinks
