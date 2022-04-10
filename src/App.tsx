1360f886b372fffc0611dd7a0c77526e88be3f3b
mark moretto <otteromkram@gmail.com>
2022-04-09 19:13:26 -0500

    downgraded react-router-dom to v5

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,42 @@
 import React from "react"
 // https://reactrouter.com/docs/en/v6/upgrading/v5#upgrade-all-switch-elements-to-routes
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
+import { BrowserRouter, Route, Switch, Link } from "react-router-dom"
 import './App.css';
 
+import Navbar from "./components/Navbar"
 import Home from "./components/Home"
 import About from "./components/About"
 
 function App() {
 	return (
-		<div className="App">
+		<React.Fragment>
+			<Navbar />
 			<div>
-				<nav>
-					<ul>
-						<li>
-							<Link to="/">Home</Link>
-						</li>
-						<li>
-							<Link to="/about">About</Link>
-						</li>					
-					</ul>
-				</nav>
+				<div>
+					<nav>
+						<ul>
+							<li>
+								<Link to="/">Home</Link>
+							</li>
+							<li>
+								<Link to="/about">About</Link>
+							</li>					
+						</ul>
+					</nav>
+				</div>
+
+				<Switch>
+					<Route exact path="/">
+						<Home />
+					</Route>
+					<Route path="/about">
+						<About />
+					</Route>
+				</Switch>
+
 			</div>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={<Home />} />
-					<Route path="/about" element={<About />} />					
-				</Routes>
-			</BrowserRouter>
-		</div>
+		</React.Fragment>
 	);
 }
 
-export default App;
+export default App
---
+++ src/components/Navbar/nav-links.js
@@ -0,0 +1,24 @@
+const navLinks = [
+    {
+        id: 1,
+        path: "/",
+        text: "Home",
+    },
+    {
+        id: 2,
+        path: "/keyword-similarity",
+        text: "Keywords",
+    },      
+    {
+        id: 3,
+        path: "/random-stopwords",
+        text: "Stopwords",
+    },    
+    {
+        id: 4,
+        path: "/about",
+        text: "About",
+    }
+]
+
+export default navLinks
